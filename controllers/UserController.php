bbbbb941259adeb853260c3c0099161e7dfa8551
209-19 <alina.okotnikova2002@gmail.com>
2022-12-05 14:15:12 +0300

    v4

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -35,4 +35,34 @@ class UserController extends FunctionController
         $user->save();//Сохранение модели в БД
         return $this->send(201, ['content'=>['code'=>201, 'message'=>'Вы зарегистрировались']]);//Отправка сообщения пользователю
     }
+
+    public function actionLogin(){
+        $request=Yii::$app->request->post();//Здесь не объект, а ассоциативный массив
+        $loginForm=new LoginForm($request);
+        if (!$loginForm->validate()) return $this->validation($loginForm);
+        $user=User::find()->where(['login'=>$request['login']])->one();
+        if (isset($user) && Yii::$app->getSecurity()->validatePassword($request['password'], $user->password)){
+            $user->tocken=Yii::$app->getSecurity()->generateRandomString();
+            $user->save(false);
+            return $this->send(200, ['content'=>['tocken'=>$user->tocken]]);
+        }
+        return $this->send(401, ['content'=>['code'=>401, 'message'=>'Неверный email или пароль']]);
+    }
+
+    /*
+     * Личный кабинет пользователя
+     */
+    public function actionAccount(){
+        $total_price=0;
+        $user=Yii::$app->user->identity; // Получить идентифицированного пользователя
+        $charts=$user->getCharts()->all();
+        $chartItems=[];
+        foreach ($charts as $chart){
+            $chart=new Chart($chart);
+            $product=new Product($chart->getProduct()->one());
+            $chartItems[]=['amount'=>$chart->amount, 'product'=>$product->name, 'image'=>$product->image, 'price'=>$product->price];
+            $total_price+=$chart->amount*$product->price;
+        }
+        return $this->send(200, ['content'=> ['user'=>$user, 'order'=>$chartItems, 'TOTAL'=>$total_price]]);
+    }
 }
